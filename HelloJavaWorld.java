9f5fb5d948045932417c52c831db0ffdd4295a53
zefangcc <52372244+zefangcc@users.noreply.github.com>
2020-11-03 21:08:45 +0800

    hello github

---
+++ HelloJavaWorld.java
@@ -0,0 +1,14 @@
+package helloJavaWorld;
+/**
+ * @(#)HelloJavaWorld.java
+ *
+ *
+ * @Seaweed
+ * @version 1.00 2020/9/2
+ */
+public class HelloJavaWorld {
+	public static void main(String[] args ) {
+		System.out.println("Hello!!!HELLO!!!");
+	}
+
+}
